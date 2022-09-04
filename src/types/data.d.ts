a3cb2d1c4a543226ad69db4c172d442d584370db
Akira Otaka <hydrangeas.project@gmail.com>
2022-09-04 13:20:39 +0000

    商品に関するtypeを追加

--- src/types/data.d.ts
+++ src/types/data.d.ts
@@ -1,3 +1,8 @@
+// 商品のカテゴリ
+export type Category = 'shoes' | 'clothes' | 'book'
+// 商品の状態
+export type Condition = 'new' | 'used'
+
 // ユーザー
 export type User = {
   id: number
@@ -6,6 +11,19 @@ export type User = {
   email: string
   profileImageUrl: string
   description: string
+}
+
+// 商品
+export type Product = {
+  id: number
+  category: Category
+  title: string
+  description: string
+  imageUrl: string
+  blurDataUrl: string
+  price: number
+  condition: Condition
+  owner: User
 }
 
 // API Context
