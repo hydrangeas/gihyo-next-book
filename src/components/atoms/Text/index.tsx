c117829dee43064c996be2d453e5554d00a6d106
Akira Otaka <hydrangeas.project@gmail.com>
2022-09-16 14:46:36 +0000

    add Text component

---
+++ src/components/atoms/Text/index.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import styled from 'styled-components';
+import type { Responsive } from '../../../types/styles';
+import {
+  toPropValue,
+  Space,
+  Color,
+  FontSize,
+  LetterSpacing,
+  LineHeight,
+} from '../../../utils/styles';
